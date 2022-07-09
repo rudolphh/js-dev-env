04942fc749e4d878f577f751673817dbe5c6df6b
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-07-08 18:03:20 -0700

    transpiling with babel (babel-node)

--- build-scripts/start-msg.js
+++ build-scripts/start-msg.js
@@ -1,3 +1,5 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
+// instead of commonJS, using Babel to transpile can now use js import statements
+import chalk from 'chalk';
 
 console.log(chalk.green('starting the app in dev mode...'));
