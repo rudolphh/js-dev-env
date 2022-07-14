747b89ad217cc9123c5675434eb1c116f2ceec87
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-07-13 17:32:49 -0700

    Generate mock data

---
+++ build-scripts/generate-mock-data.js
@@ -0,0 +1,23 @@
+/*
+  This script generates mock data for local development.
+  This way you don't have to point to an actual API,
+  but you can enjoy realistic, but randomized data,
+  and rapid page loads due to local, static data
+*/
+
+/* eslint-disable no-console */
+
+import { extend, generate } from 'json-schema-faker';
+
+import chalk from 'chalk';
+import fs from 'fs';
+import { schema } from './mock-data-schema';
+
+// Extend JSF with the fake libs you want to use (faker, chance, randomexp)
+extend("faker", () => require('faker'));
+const json = JSON.stringify(generate(schema));
+
+fs.writeFile("./src/api/db.json", json, function (err) {
+  if(err) return console.log(chalk.red(err));
+  console.log(chalk.green('Mock data generated'));
+})
